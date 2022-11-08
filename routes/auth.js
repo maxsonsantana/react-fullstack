3c4628443912d45996996ba32dfbf0b96870321d
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-11-07 23:46:02 -0300

    Login 1

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/UserModel");
 const bcrypt = require('bcryptjs');
 const validatorRegisterInput = require('../validation/registerValidation');
+const jwt = require('jsonwebtoken');
 
 
 //@route GET /api/auth/test
@@ -34,18 +35,79 @@ router.post("/register", async(req, res) => {
 
         //hash the password
         const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+        //Create a new user
         const newUser = new User({
             email: req.body.email,
             password: hashedPassword,
             name: req.body.name
         });
+
+        //Save the user to the database
         const savedUser = await newUser.save();
-        return res.json(savedUser);
+
+        //Não retorna o Password
+        const usertoReturn = { ...savedUser._doc }
+        delete usertoReturn.password;
+
+        //Return the new user
+        return res.json(usertoReturn);
     }catch(err){
         //TODO
         console.log(err);
         res.status(500).send(err.message);
     }
-})
+});
+
+//@route POST /api//auth/login
+//@desc  Login user and return a access token
+//@access Public
+router.post("/login", async (req, res)=>{
+    try{
+        //Check for the user
+        const user = await User.findOne({
+            email: new RegExp("^"+ req.body.email + "$", "i"),
+        });
+        if(!user){
+            return res
+            .status(400)
+            .json({ error: 'Existe um problema com suas credenciais de Login'});
+        }
+
+        const passwordMatch = await bcrypt.compare(
+            req.body.password, 
+            user.password
+        );
+
+        if(!passwordMatch){
+            return res
+            .status(400)
+            .json({ error: 'Existe um problema com suas credenciais de Login'});
+        }
+
+        const payload = { userId: user._id };
+        const token = jwt.sign(payload, process.env.JWT_SECRET,{
+            expiresIn: "7d"
+        });
+
+        res.cookie("access-token", token,{
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000 ),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production"
+        });
+
+        const usertoReturn = { ...user._doc};
+        delete usertoReturn.password;
+
+        return res.json({
+            token: token,
+            user: usertoReturn,
+        });
+
+    }catch(err){
+        console.log(err);
+        return res.status(500).send(err.message);
+    }
+});
 
 module.exports = router;
